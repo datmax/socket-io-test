21de4a82800bb45f5f73e9c588480b3353d05e11
datmax <unrusso@live.com>
2019-02-08 20:02:02 +0100

    idk what i did there

--- index.js
+++ index.js
@@ -8,26 +8,38 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+
+
 io.on("connection", function(socket){
+    socket.join("home");
+    console.log(socket.rooms);
     users[socket.id] = {
         id: socket.id,
         userName: ""
     }
-    console.log(users);
     io.emit("connectionChange", {msg: "user connected", userList: users});
     socket.on('disconnect', function(){
+        io.emit("connectionChange", {msg: users[socket.id].userName + " disconnected.", userList: users});
         delete users[socket.id];
-        io.emit("connectionChange", {msg:"user disconnected.", userList: users});
     });
     socket.on("typing", function(user){
-        console.log("hello");
+        //console.log("hello");
         socket.broadcast.emit("typing", user);
     });
     socket.on('chat message', function(payload){
         socket.broadcast.emit("chat message", payload);
       });
     socket.on("save name", function(name){
-        socket[socket.id].userName = name;
+        users[socket.id].userName = name;
+        console.log(users);
+    });
+    socket.on("change room", function(room) {
+        console.log("changing room");
+        socket.leave("home");
+        socket.join(room, ()=>{
+            console.log(io.sockets.adapter.rooms);
+
+        });
     })
 })
 
